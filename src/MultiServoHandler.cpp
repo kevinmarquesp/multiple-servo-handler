238cc5fc0a32b1f3212c58b1701f61c86e298d82
kevinmarquesp <kevinmarqueskmp@gmail.com>
2022-12-18 14:17:05 -0300

    feat: Move more than one servo at the same time

--- src/MultiServoHandler.cpp
+++ src/MultiServoHandler.cpp
@@ -8,17 +8,15 @@ void MultiServoHandler::_validadeDegValue(uint8_t* deg, uint8_t index) {
         *deg = this->_maxDeg[index];
 }
 
-void MultiServoHandler::_waitTask(uint64_t* task, uint16_t sleep, void (*exec)(void)) {
-    if (this->_cMill - *task >= sleep) {
-        *task = this->_cMill;
-        exec();
-    }
-}
-
 void MultiServoHandler::config(uint8_t amount, uint8_t* pins, uint8_t* minDeg, uint8_t* maxDeg) {
     this->_minDeg = new uint8_t[amount];
     this->_maxDeg = new uint8_t[amount];
     this->_tasks = new uint64_t[amount];
+
+    this->_currPos = new uint8_t[amount];
+    this->_lock = new bool[amount];
+    this->_amount = amount;
+
     this->Motor = new Servo[amount];
 
     for (uint8_t i = 0; i < amount; i++) {
@@ -26,46 +24,35 @@ void MultiServoHandler::config(uint8_t amount, uint8_t* pins, uint8_t* minDeg, u
         this->_maxDeg[i] = maxDeg[i];
 
         this->Motor[i].attach(pins[i]);
-        this->Motor[i].write(minDeg[i]);
+        this->_currPos[i] = minDeg[i];
     }
+
+    this->loopUpdate();
 }
 
 void MultiServoHandler::loopUpdate(void) {
     this->_cMill = millis();
+
+    for (uint8_t i = 0; i < this->_amount; i++) {
+        this->Motor[i].write(this->_currPos[i]);
+    }
 }
 
 void MultiServoHandler::move(uint8_t index, uint8_t deg, uint16_t sleep = 0) {
     this->_validadeDegValue(&deg, index);
-    uint8_t cPos = this->Motor[index].read();
 
-    if (not sleep or deg == cPos)
+    if (not sleep or deg == this->_currPos[index]) {
         return;
-
-    if (this->_cMill - this->_tasks[index] >= sleep) {
-
-        this->_tasks[index] = this->_cMill;
-        this->Motor[index].write(cPos);
-
-        if (deg > cPos) {
-            this->Motor[index].write(cPos);
-            cPos++;
-
-        } else if (deg < cPos) {
-            cPos--;
-        }
-
     }
 
-    /* if (deg > cPos) { */
-    /*     for (cPos; cPos <= deg; cPos++) { */
-    /*         this->Motor[index].write(cPos); */
-    /*         delay(waitDelay); */
-    /*     } */
+    if (this->_cMill - this->_tasks[index] >= sleep) {
+        this->_tasks[index] = this->_cMill;
 
-    /* } else if (deg < cPos) { */
-    /*     for (cPos; cPos >= deg; cPos--) { */
-    /*         this->Motor[index].write(cPos); */
-    /*         delay(waitDelay); */
-    /*     } */
-    /* } */
+        if (deg > this->_currPos[index])
+            this->_currPos[index]++;
+
+        else if (deg < this->_currPos[index])
+            this->_currPos[index]--;
+
+    }
 }
--- src/MultiServoHandler.h
+++ src/MultiServoHandler.h
@@ -8,11 +8,14 @@ class MultiServoHandler {
 private:
     uint8_t* _minDeg;
     uint8_t* _maxDeg;
+    uint8_t* _currPos;
     uint64_t* _tasks;
+    bool* _lock;
+
     uint64_t _cMill;
+    uint8_t _amount;
 
     void _validadeDegValue(uint8_t*, uint8_t);
-    void _waitTask(uint64_t*, uint16_t, void (*)(void));
 
 public:
     Servo* Motor;
